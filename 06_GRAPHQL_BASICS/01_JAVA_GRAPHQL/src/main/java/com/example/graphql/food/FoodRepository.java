539411f77f295baa13b8503de77f3bf2bb38f96b
CrispenGari <Crispengari@gmail.com>
2021-12-03 12:30:10 +0200

    removed-@graphql

--- 06_GRAPHQL_BASICS/01_JAVA_GRAPHQL/src/main/java/com/example/graphql/food/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.graphql.food;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FoodRepository extends JpaRepository<Food, Long> {
-
-}
